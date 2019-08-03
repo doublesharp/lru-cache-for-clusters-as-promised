c6e97d1eb4ca0996b48e78ee100affd4e4cbf9f5
Justin Silver <jsilver@doublesharp.com>
2019-08-03 10:56:25 -0700

    v1.5.19
    fix nyc coverage

---
+++ test/nyc.js
@@ -0,0 +1,9 @@
+// this fixes the coverage for nyc badges
+require('../lib/nyc');
+
+
+describe('Fix for nyc badges', () => {
+  it('should load a file from lib', (done) => {
+    done();
+  });
+});
